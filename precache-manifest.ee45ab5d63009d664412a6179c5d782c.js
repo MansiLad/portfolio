bf1cbc44b468d8bdb0d392c1ddd00ba7966322a2
MansiLad <ladmansi7@gmail.com>
2024-07-12 19:53:04 -0400

    Updates

---
+++ precache-manifest.ee45ab5d63009d664412a6179c5d782c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590731a606061f8f96f9283b46dcc8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b752c72bf48f8cd9be0",
+    "url": "/static/css/2.31b0aa11.chunk.css"
+  },
+  {
+    "revision": "83d82b79bd25d0aa8a0b",
+    "url": "/static/css/main.f611c18d.chunk.css"
+  },
+  {
+    "revision": "1b752c72bf48f8cd9be0",
+    "url": "/static/js/2.7f5d70f0.chunk.js"
+  },
+  {
+    "revision": "891fbd39d4c3dbed1a31ef537118c9a6",
+    "url": "/static/js/2.7f5d70f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d82b79bd25d0aa8a0b",
+    "url": "/static/js/main.05ec8e54.chunk.js"
+  },
+  {
+    "revision": "07e556cc98188fae9926",
+    "url": "/static/js/runtime-main.bbbb3173.js"
+  }
+]);
